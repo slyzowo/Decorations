6a79ca2fd57308f2f79f2df051d3389220fc8c6e
slyzOwO <lilithhimes@gmail.com>
2026-02-03 11:20:12 -0600

    adding glasses, untested

--- src/main/java/net/slyzowo/decorations/block/ModBlocks.java
+++ src/main/java/net/slyzowo/decorations/block/ModBlocks.java
@@ -1,8 +1,6 @@
 package net.slyzowo.decorations.block;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.MapColor;
+import net.minecraft.block.*;
 import net.minecraft.block.enums.NoteBlockInstrument;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -30,13 +28,103 @@ public class ModBlocks {
                   .sounds(BlockSoundGroup.WOOL)
                   .burnable()));
 
-  public static final Block GLASS = registerBlock("transgender_wool",
-          new Block(AbstractBlock.Settings.create()
-                  .mapColor(MapColor.BLUE)
-                  .instrument(NoteBlockInstrument.GUITAR)
-                  .strength(0.8F)
-                  .sounds(BlockSoundGroup.WOOL)
-                  .burnable()));
+  public static final Block CLEAR_GLASS = registerBlock("clear_glass",
+          new TransparentBlock(
+                  AbstractBlock.Settings.create()
+                          .instrument(NoteBlockInstrument.HAT)
+                          .strength(0.3F)
+                          .sounds(BlockSoundGroup.GLASS)
+                          .nonOpaque()
+                          .allowsSpawning(Blocks::never)
+                          .solidBlock(Blocks::never)
+                          .suffocates(Blocks::never)
+                          .blockVision(Blocks::never)
+          ));
+
+  public static final Block SCRATCHED_GLASS = registerBlock("scratched_glass",
+          new TransparentBlock(
+                  AbstractBlock.Settings.create()
+                          .instrument(NoteBlockInstrument.HAT)
+                          .strength(0.3F)
+                          .sounds(BlockSoundGroup.GLASS)
+                          .nonOpaque()
+                          .allowsSpawning(Blocks::never)
+                          .solidBlock(Blocks::never)
+                          .suffocates(Blocks::never)
+                          .blockVision(Blocks::never)
+          ));
+
+  public static final Block SCRATCHED_GLASS_PANE = registerBlock("scratched_glass_pane",
+          new PaneBlock(AbstractBlock.Settings.create()
+                  .instrument(NoteBlockInstrument.HAT)
+                  .strength(0.3F)
+                  .sounds(BlockSoundGroup.GLASS)
+                  .nonOpaque()));
+
+  public static final Block COPPER_REINFORCED_GLASS = registerBlock("copper_reinforced_glass",
+          new TransparentBlock(
+                  AbstractBlock.Settings.create()
+                          .instrument(NoteBlockInstrument.HAT)
+                          .strength(0.3F)
+                          .sounds(BlockSoundGroup.GLASS)
+                          .nonOpaque()
+                          .allowsSpawning(Blocks::never)
+                          .solidBlock(Blocks::never)
+                          .suffocates(Blocks::never)
+                          .blockVision(Blocks::never)
+          ));
+
+  public static final Block IRON_REINFORCED_GLASS = registerBlock("iron_reinforced_glass",
+          new TransparentBlock(
+                  AbstractBlock.Settings.create()
+                          .instrument(NoteBlockInstrument.HAT)
+                          .strength(0.3F)
+                          .sounds(BlockSoundGroup.GLASS)
+                          .nonOpaque()
+                          .allowsSpawning(Blocks::never)
+                          .solidBlock(Blocks::never)
+                          .suffocates(Blocks::never)
+                          .blockVision(Blocks::never)
+          ));
+
+  public static final Block GOLD_REINFORCED_GLASS = registerBlock("gold_reinforced_glass",
+          new TransparentBlock(
+                  AbstractBlock.Settings.create()
+                          .instrument(NoteBlockInstrument.HAT)
+                          .strength(0.3F)
+                          .sounds(BlockSoundGroup.GLASS)
+                          .nonOpaque()
+                          .allowsSpawning(Blocks::never)
+                          .solidBlock(Blocks::never)
+                          .suffocates(Blocks::never)
+                          .blockVision(Blocks::never)
+          ));
+
+  public static final Block DIAMOND_REINFORCED_GLASS = registerBlock("diamond_reinforced_glass",
+          new TransparentBlock(
+                  AbstractBlock.Settings.create()
+                          .instrument(NoteBlockInstrument.HAT)
+                          .strength(0.3F)
+                          .sounds(BlockSoundGroup.GLASS)
+                          .nonOpaque()
+                          .allowsSpawning(Blocks::never)
+                          .solidBlock(Blocks::never)
+                          .suffocates(Blocks::never)
+                          .blockVision(Blocks::never)
+          ));
+
+  public static final Block OBSIDIAN_REINFORCED_GLASS = registerBlock("obsidian_reinforced_glass",
+          new TransparentBlock(
+                  AbstractBlock.Settings.create()
+                          .instrument(NoteBlockInstrument.HAT)
+                          .strength(0.3F)
+                          .sounds(BlockSoundGroup.GLASS)
+                          .nonOpaque()
+                          .allowsSpawning(Blocks::never)
+                          .solidBlock(Blocks::never)
+                          .suffocates(Blocks::never)
+                          .blockVision(Blocks::never)
+          ));
 
   private static Block registerBlock(String name, Block block){
     registerBlockItem(name, block);
